c227064cdefe5ce196e1aea445a21cd624a2254f
Rainer Bareiß <Heavy02011@users.noreply.github.com>
2021-09-11 11:42:49 +0200

    Update install_install0.sh

--- install_install0.sh
+++ install_install0.sh
@@ -3,11 +3,13 @@
 # python 3.
 https://www.liquidweb.com/kb/how-to-install-and-update-python-to-3-9-in-ubuntu/
 
+## install python3.9
 ```
 sudo apt update
 sudo apt install python3.9 python3.9-venv python3.9-dev
 python3.9 -V
 ```
+## make python3.9 the defaul
 
 ```
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
@@ -15,9 +17,9 @@ sudo update-alternatives --install /usr/bin/python3.6.9 python /usr/bin/python3 
 sudo update-alternatives --install /usr/bin/python3 python /usr/bin/python3.9 3
 sudo update-alternatives --config python
 python3
-   95  sudo update-alternatives --config python
-   96  pip3 install virtualenv
-   97  python3 -m virtualenv -p python3 env --system-site-packages
-   98  . .bashrc
-
+sudo update-alternatives --config python
+pip3 install virtualenv
+python3 -m virtualenv -p python3 env --system-site-packages
+. .bashrc
+```
 
